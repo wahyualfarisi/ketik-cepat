0d13371444711ea0348b9bc6429e96d77aab0c49
wahyu alfarisi <wahyualfarisi@gmail.com>
2021-08-28 22:01:03 +0700

    reformat app.js

--- dist/app.js
+++ dist/app.js
@@ -1,107 +1,125 @@
-window.addEventListener('load', initializeApp);
+const App = ( () => {
+    //GLOBAL VARIABEL
+    let time  = 5;
+    let score = 0;
+    let isPlaying;
 
 
-//GLOBAL VARIABEL
-let time  = 5;
-let score = 0;
-let isPlaying;
+    const textInput     = document.querySelector('#input-keyword');
+    const currentWord   = document.querySelector('#current-word');
+    const scoreDisplay  = document.querySelector('#score');
+    const timeDisplay   = document.querySelector('#time');
+    const message       = document.querySelector('#message');
+    const second        = document.querySelector('#seconds');
+    const showTime      = document.querySelector('#show-time');
+
+    const wordData = [
+        'javascript app',
+        'node js',
+        'mongodb',
+        'pwa',
+        'firebase',
+        'socket.io',
+        'react',
+        'react native',
+        'heroku',
+        'express js',
+        'mocha',
+        'jest',
+        'amazone',
+        'Vue JS'
+    ];
+
+    const eventListener = () => {
+
+        document.querySelector('#change-speed').addEventListener('change', function(){
+            if(this.value !== ''){
+                time = parseInt(this.value) + 1;
+                showWord(wordData);
+                showTime.innerHTML = `Kecepatan mengetik ${time - 1}. detik`;
+            }else{
+                time = 6;
+            }
+           
+        });
 
 
-const textInput     = document.querySelector('#input-keyword');
-const currentWord   = document.querySelector('#current-word');
-const scoreDisplay  = document.querySelector('#score');
-const timeDisplay   = document.querySelector('#time');
-const message       = document.querySelector('#message');
-const second        = document.querySelector('#seconds');
-const showTime      = document.querySelector('#show-time');
+    }
 
-const wordData = [
-    'javascript app',
-    'node js',
-    'mongodb',
-    'pwa',
-    'firebase',
-    'socket.io',
-    'react',
-    'react native',
-    'heroku',
-    'express js',
-    'mocha',
-    'jest',
-    'amazone'
-];
-
-function initializeApp()
-{
-    showWord(wordData);
-    showTime.innerHTML = `Kecepatan mengetik <b>${time}</b>. detik`;
-    textInput.addEventListener('input', startPlay);
-
-    setInterval(countDown, 1000);
-    setInterval(checkStatusPlay, 50);
-}
-
-document.querySelector('#change-speed').addEventListener('change', function(){
-    if(this.value !== ''){
-        time = parseInt(this.value) + 1;
+    const initializeApp = () => {
         showWord(wordData);
-        showTime.innerHTML = `Kecepatan mengetik ${time - 1}. detik`;
-    }else{
-        time = 6;
-    }
-   
-});
+        showTime.innerHTML = `Kecepatan mengetik <b>${time}</b>. detik`;
+        textInput.addEventListener('input', startPlay);
 
-function startPlay()
-{
-    if(matchWord()){
-        isPlaying = true;
-        time = 6;
-        showWord(wordData);
-        textInput.value = '';
-        score++;
+        setInterval(countDown, 1000);
+        setInterval(checkStatusPlay, 50);
     }
 
-    scoreDisplay.innerHTML = score;
-}
-
-function matchWord()
-{
-    if(textInput.value === currentWord.innerHTML)
-    {
-        message.style.color = 'green';
-        message.innerHTML = 'Correct!';
-        return true;
-    }else{
-        message.innerHTML = '';
-        return false;
+    const showWord = (words) => {
+        const random = Math.floor(Math.random() * words.length);
+        currentWord.innerHTML = words[random];
     }
-}
 
-function showWord(words)
-{
-    const random = Math.floor(Math.random() * words.length);
-    currentWord.innerHTML = words[random];
-}
-
-function countDown()
-{
-    if(time > 0)
-    {
-        time--;
-    }else if(time === 0)
-    {
-        isPlaying = false;
+    const startPlay = () => {
+        if(matchWord()){
+            isPlaying = true;
+            time = 6;
+            showWord(wordData);
+            textInput.value = '';
+            score++;
+        }
+    
+        scoreDisplay.innerHTML = score;
     }
-    timeDisplay.innerHTML = time;
-}
 
-function checkStatusPlay()
-{
-    if(!isPlaying && time === 0)
-    {
-        message.style.color = 'red';
-        message.innerHTML = 'Game Over';
-        score = 0;
+
+    const  matchWord = () => {
+        if(textInput.value === currentWord.innerHTML)
+        {
+            message.style.color = 'green';
+            message.innerHTML = 'Correct!';
+            return true;
+        }else{
+            message.innerHTML = '';
+            return false;
+        }
     }
-}
+
+    const countDown = () => {
+        if(time > 0)
+        {
+            time--;
+        }else if(time === 0)
+        {
+            isPlaying = false;
+        }
+        timeDisplay.innerHTML = time;
+    }
+
+    const checkStatusPlay = () => {
+        if(!isPlaying && time === 0)
+        {
+            message.style.color = 'red';
+            message.innerHTML = 'Game Over';
+            score = 0;
+        }
+    }
+
+
+
+
+    return {
+        init: () => {
+            console.log('init apps');
+            eventListener();
+            initializeApp();
+        }
+    }
+})()
+
+App.init();
+
+
+
+
+
